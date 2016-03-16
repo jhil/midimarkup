1cfda0f71ac59f882112c3b94a8f8fe01eb92fb3
nlaz <nikolazaris@utexas.edu>
2016-03-16 12:34:12 -0500

    fix jank

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,34 +1,3 @@
-var soundPlay = new Audio("/img/play.mp3"),
-    soundStop = new Audio("/img/stop.mp3"),
-    sayInstrument = new SpeechSynthesisUtterance('trumpet');
-
-
-
-$('#play-toggle').on({
-  'click': function(){
-    soundPlay.play();
-    playTrack.play();
-  }
-});
-
-$('#stop-toggle').on({
-  'click': function(){
-    soundStop.play();
-  }
-});
-
-$( document ).keydown(function (e) {
-  if(e.shiftKey && e.keyCode == 13){
-    if($('#play-toggle').children("img").attr('src') == '/img/icon-play.svg') {
-      soundStop.play();
-      playTrack.play();
-    } else {
-      soundPlay.play();
-    }
-  }
-});
-
-
 $(document).keyup(function(e) {
   switch(e.which) {
     case 37: // left
