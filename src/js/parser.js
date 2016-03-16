9625412daeece7f8f4159299fd9f2550dbea1783
nlaz <nikolazaris@utexas.edu>
2016-03-16 03:38:36 -0500

    parse notes

--- src/js/parser.js
+++ src/js/parser.js
@@ -20,11 +20,88 @@ var play = function () {
   $(".track").each(function( index ) {
     var instrument = $(this).find(".track-instrument").text();
     var markup = $(this).find(".track-markup").text();
-    console.log(instrument + ": " + markup);
-  });
-  
-  var instrument = soundfont.instrument('acoustic_grand_piano');
-  instrument.onready(function() {
-    instrument.play('C4', 0);
+    var keys = parseMarkup(markup);
+
+    var instrument = soundfont.instrument('acoustic_grand_piano');
+    instrument.onready( function() {
+      instrument.play('C4', 0);
+    });
+
   });
 };
+
+var parseMarkup = function( markup ) {
+  var output = [],
+      notes = markup.toUpperCase().split(" ");
+
+  for (var x in notes) {  
+    var key = noteToKey(notes[x]);
+    if (key) {
+      key["key"] = keyToNote(key["key"])
+      output.push(key);
+    } 
+  }
+
+  return output; // i.e. { key: "Cb4", duration: 1 }
+}
+
+var noteToKey = function( note ) {
+  if (note.length > 3)
+    return;
+
+  var key = -1,
+      duration = 1;
+
+  for (var x in note) {
+    var cmd = note.charAt(x);
+    switch( cmd ) {
+      case 'A':
+        key = 69;
+        break;
+      case 'B':
+        key = 71;
+        break;
+      case 'C':
+        key = 60;
+        break;
+      case 'D':
+        key = 62;
+        break;
+      case 'E':
+        key = 64;
+        break;
+      case 'F':
+        key = 65;
+        break;
+      case 'G':
+        key = 67;
+        break;
+      case '+':
+        if (key === -1) return;
+        key++;
+        break;
+      case '-':
+        if (key === -1) return;
+        key--;
+        break;
+      case '1':
+      case '2':
+      case '4':
+      case '8':
+        duration = parseInt(cmd);
+        break;
+      default:
+        return;
+    } 
+  }
+  console.log(key + ": " + duration); 
+  return { "key": key, "duration": duration };
+}
+
+var keyToNote = function ( key ) {
+  var octave = 4;
+  var A0 = 0x15; // first note
+  var C8 = 0x6C; // last note
+  var number2key = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+  return number2key[ key % 12] + octave;
+}
