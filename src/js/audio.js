d9fcbdba079cad03f3f32e521d8adc2132b53578
Jeff Hilnbrand <jahilnbrand@gmail.com>
2016-03-16 08:21:04 -0500

    starting audio

---
+++ src/js/audio.js
@@ -0,0 +1,37 @@
+var soundPlay = new Audio("/img/play.wav");
+var soundStop = new Audio("/img/stop.wav");
+var sayInstrument = new SpeechSynthesisUtterance('trumpet');
+
+
+$('#play-toggle').on({
+    'click': function(){
+      soundPlay.play();
+    }
+});
+
+$('#stop-toggle').on({
+    'click': function(){
+      soundStop.play();
+    }
+});
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        break;
+
+        case 38: // up
+        window.speechSynthesis.speak(sayInstrument);
+        break;
+
+        case 39: // right
+        break;
+
+        case 40: // down
+        window.speechSynthesis.speak(sayInstrument);
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
